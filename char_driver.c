6e54473b41be570f5a811f01c981cd3fcc6f42bd
MidhunManu <midhunmanu384@gmail.com>
2026-03-30 17:59:43 +0530

    fix: syntax

--- char_driver.c
+++ char_driver.c
@@ -2,15 +2,16 @@
 #include <linux/init.h>
 
 int char_driver_init(void) {
-	printk(KERN_INFO "hello");
-	return 0;
+		printk(KERN_INFO "hello\n");
+		return 0;
 }
 
-void char_driver_exiti(void) {
-	printk(KERN_INFO("bye");
+void char_driver_exit(void) {
+		printk(KERN_INFO "bye\n");
 }
 
 module_init(char_driver_init);
 module_exit(char_driver_exit);
+
 MODULE_AUTHOR("MidhunManu");
 MODULE_LICENSE("GPL");
